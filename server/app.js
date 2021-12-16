200ae012912125a24a220f48d14be96421eeedff
{krism01} <{krism01@naver.com}>
2021-12-16 11:41:59 +0900

    mongoose connection 하기 전에 commit

--- server/app.js
+++ server/app.js
@@ -1,36 +1,39 @@
-// var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var toDoListRouter = require('./routes/toDoList');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const toDoListRouter = require('./routes/toDoList');
 
-var app = express();
+const app = express();
 const PORT = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// middleware
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
 
 // router
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/todo', toDoListRouter);
 
+// test
+app.get('/', (req, res) => {
+  res.send("run!")
+})
+
 // app.listen
 app.listen(PORT, () => {
-  console.log(`connected server PORT is: ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
 })
 
 module.exports = app;
