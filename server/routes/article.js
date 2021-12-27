f57b9e95f9e3bf610dd05ef891eaec171401af07
{krism01} <{krism01@naver.com}>
2021-12-27 18:21:59 +0900

    feature article crud

--- server/routes/article.js
+++ server/routes/article.js
@@ -9,4 +9,41 @@ const articleCreate = async (req, res) => {
   res.send(newArticle);
 };
 
-module.exports = { articleCreate }
+// read, articleRead
+const articleRead = async (req, res) => {
+  const article = await model.Article.find({});
+  console.log(article);
+  res.send(article);
+};
+
+// update, articleUpdate
+const articleUpdate = async (req, res) => {
+  const { id, content } = req.body;
+  const article = await model.Article.findByIdAndUpdate(id, { content }, { new: true });
+  console.log(article);
+  res.send(article);
+};
+
+// Hard delete, articleDelete
+const articleDelete = async (req, res) => {
+  const { id } = req.params;
+  const article = await model.Article.findByIdAndDelete(id);
+  console.log(article);
+  res.send(article);
+};
+
+// detail
+const articleDetail = async (req, res) => {
+  const { id } = req.params;
+  const article = await model.Article.findById(id);
+  console.log(article);
+  res.send(article);
+};
+
+module.exports = {
+  articleCreate,
+  articleDetail,
+  articleUpdate,
+  articleRead,
+  articleDelete,
+}
