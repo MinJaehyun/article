98d7c30924dd98c9bc1e65eb211f4ba09994c20e
{krism01} <{krism01@naver.com}>
2021-12-31 11:38:25 +0900

    fix app.js

--- server/routes/article.js
+++ server/routes/article.js
@@ -5,14 +5,14 @@ const articleCreate = async (req, res) => {
   // client 에서 보낸 데이터를 받아, model 에 저장한다.
   const { content } = req.body;
   const newArticle = await model.Article({ content }).save();
-  console.log(newArticle);
+  // console.log(newArticle);
   res.send(newArticle);
 };
 
 // read, articleRead
 const articleRead = async (req, res) => {
   const article = await model.Article.find({});
-  console.log(article);
+  // console.log(article);
   res.send(article);
 };
 
@@ -20,7 +20,7 @@ const articleRead = async (req, res) => {
 const articleUpdate = async (req, res) => {
   const { id, content } = req.body;
   const article = await model.Article.findByIdAndUpdate(id, { content }, { new: true });
-  console.log(article);
+  // console.log(article);
   res.send(article);
 };
 
@@ -28,7 +28,7 @@ const articleUpdate = async (req, res) => {
 const articleDelete = async (req, res) => {
   const { id } = req.params;
   const article = await model.Article.findByIdAndDelete(id);
-  console.log(article);
+  // console.log(article);
   res.send(article);
 };
 
@@ -36,7 +36,7 @@ const articleDelete = async (req, res) => {
 const articleDetail = async (req, res) => {
   const { id } = req.params;
   const article = await model.Article.findById(id);
-  console.log(article);
+  // console.log(article);
   res.send(article);
 };
 
